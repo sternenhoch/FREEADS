f6db02024081268f926d3e9437cc6239026f863c
mishkaua <marishka.pichik@gmail.com>
2023-10-31 11:31:42 +0100

    Added completed signup form

--- mm-ads/resources/views/signup.blade.php
+++ mm-ads/resources/views/signup.blade.php
@@ -1,5 +1,34 @@
 <x-header_footer>
 
-    <h1>Signup Form</h1>
+    <div class="col-lg-5 pl-lg-5 pb-3 py-lg-5">
+        <form action="#" method="POST" id="registration-form">
+          <div class="form-group">
+            <label for="login" class="text-muted mb-1"><small>Login</small></label>
+            <input name="login" id="login" class="form-control" type="text" placeholder="Pick a login" autocomplete="off" />
+          </div>
+
+          <div class="form-group">
+            <label for="email" class="text-muted mb-1"><small>Email</small></label>
+            <input name="email" id="email" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" required />
+          </div>
+
+          <div class="form-group">
+            <label for="password" class="text-muted mb-1"><small>Password</small></label>
+            <input name="password" id="password" class="form-control" type="password" placeholder="Create a password" minlength="5" required />
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation" class="text-muted mb-1"><small>Confirm Password</small></label>
+            <input name="password_confirmation" id="password_confirmation" class="form-control" type="password" placeholder="Confirm password" minlength="5" required/>
+          </div>
+
+          <div class="form-group" class="text-muted mb-1">
+            <label for="phone_number"><small>Phone number</small></label>
+            <input name="phone_number" type="tel" id="phone_number" class="form-control" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" placeholder="01 02 03 04 05">
+            </div>
+
+          <button type="submit" class="py-3 mt-4 btn btn-success btn-block">Create your account</button>
+        </form>
+      </div>
 
 </x-header_footer>
